a47ce635bad536a10401bcbab491641b9199698d
seven05 <ock1997@gmail.com>
2024-10-29 23:57:05 +0900

    CRUD

--- app.ts
+++ app.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from 'express';
-import { connect_db } from './schemas'; 
-
+import { connect_db } from './schemas';
+import routes from "./routes"
 const app = express();
 const port = 3000;
 
 connect_db();
+
+app.use(express.json());
+
+app.use('/posts', routes.post);
+app.use('/comments', routes.comments);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('TypeScript, Node.js, Express, mongodb connect');
---
+++ routes/index.ts
@@ -0,0 +1,5 @@
+import postRoutes from './posts';
+import commentRoutes from './comments';
+
+export default {post : postRoutes, comments : commentRoutes}
+
